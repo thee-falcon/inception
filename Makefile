220925c5d819abcb601e44b0a13f263cd8c2f420
thee-falcon <omar.makran.10@gmail.com>
2024-09-30 22:13:35 +0100

    finished

--- Makefile
+++ Makefile
@@ -10,27 +10,25 @@
 #                                                                              #
 # **************************************************************************** #
 
-# Target run the docker-compose file which starts up the conatiners
-all:
-	@docker-compose -f ./srcs/docker-compose.yml up
-# builds and starts the containers using the Docker Compose
-# --build to rebuild of the images before starting the containers
-build:
-	@docker-compose -f ./srcs/docker-compose.yml up  --build
-# stopd and remove the containers
-down:
-	@docker-compose -f ./srcs/docker-compose.yml down
-# remove the unused Docker data like stopped containers, unused networks, images and build cache
-clean: down
-	@docker system prune -af
-# stops all running containers and the unused Docker data
-fclean:
-	@docker stop $$(docker ps -qa)
-	@docker volume prune -f
-	@docker network prune --force
-	@docker system prune -af
-# stratup the containers from scratch, It's used fully reset the environment
-re:	fclean all
-	@docker-compose -f ./srcs/docker-compose.yml up  --build
+NAME=inception
 
-.PHONY	: all build down clean fclean re
+VOLUME="/home/omakran/data"
+
+all:
+	@echo "setting up the infrastructure"
+	@mkdir -p $(VOLUME)/wordpress
+	@mkdir -p $(VOLUME)/mariadb
+	@docker-compose -p $(NAME) -f ./srcs/docker-compose.yml up --build -d
+
+clean:
+	@docker-compose -p $(NAME) -f ./srcs/docker-compose.yml down
+
+fclean: clean
+	@docker volume rm -f $$(docker volume ls -q)
+	@docker rmi -f $$(docker images -qa)
+	@sudo rm -rf $(VOLUME)/wordpress
+	@sudo rm -rf $(VOLUME)/mariadb
+
+re: fclean all
+
+.PHONY: all clean fclean re
--- srcs/requirements/wordpress/conf/create_wordpress.sh
+++ srcs/requirements/wordpress/conf/create_wordpress.sh
@@ -1,46 +1,22 @@
-#!bin/sh
+#!/bin/sh
 cd /var/www/html
 
-# download the WP-CLI (WordPress Command Line Interface) using curl
-# the -O options saves the file with its original name
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
-# make the file executable
 chmod +x wp-cli.phar
 
-# move the wp-cli.phar file to /usr/local/bin/  and rename it to wp
-# so it can be used as a command-line tool system-wide
 mv wp-cli.phar /usr/local/bin/wp
 
-# use the wp command to download the core wordpress files into the current directory(/var/www/html)
 wp core download
 
-# rename the default config-sample.php file to wp-config.php
-# which is the main configuration file for wordpress
 mv wp-config-sample.php wp-config.php
-
-# replace placeholders in wp-config.php with actual environment variables (DB_NAME, DB_PASS, WP_USER)
 sed -i "s/database_name_here/$DB_NAME/1" wp-config.php
 sed -i "s/username_here/$WP_USER/1" wp-config.php
 sed -i "s/password_here/$DB_PASS/1" wp-config.php
-
-# replace localhost with mariadb in wp-config.php (mariadb refers to the service name in the docker-compose file)
 sed -i "s/localhost/mariadb/1" wp-config.php
 
-# use WP-CLI to install wordpress with specific settings:
-# --url=$DOMAIN_NAME: the URL where the wordpress will be accessible
-# --title=$WP_USER: the title of wordpress site
-# --admin_user=$WP_USER: the wordpress admin username
-# --admin_password=$DB_PASS: the wordpress admin password
-# --admin_email=$WP_ADMIN_EMAIL: the wordpress admin email
-# --skip-email: skip sending a confirmation email to the admin
-# --allow-root: allow the commmand to run as root user
 wp core install --url=$DOMAIN_NAME --title=$WP_USER --admin_user=$WP_USER --admin_password=$DB_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 
-# use WP-CLI to create a new user with the role of author
-# $DB_USER: is the username, and $WP_USER_EMAIL: is the email address associated with this user
-# the user's password is set to $DB_PASS
 wp user create $DB_USER $WP_USER_EMAIL --role=author --user_pass=$DB_PASS --allow-root
 
-# start PHP-FPM (FastCGI Process Manager) in the foreground (-F) and allow the root to run it (-R)
 /usr/sbin/php-fpm7 -F -R
